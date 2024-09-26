e9213fc340faa0726970f362e50dc404d7e5953f
nandeeswarbadugu <169081821+nandeeswarbadugu@users.noreply.github.com>
2024-09-25 19:04:05 -0500

    added create mint function

--- src/components/TokenLaunchpad.jsx
+++ src/components/TokenLaunchpad.jsx
@@ -1,12 +1,41 @@
+import { createMint } from '@solana/spl-token';
+import { useWallet, useConnection} from '@solana/wallet-adapter-react';
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import { Keypair } from '@solana/web3.js';
 
 export function TokenLaunchpad() {
 
-    function createToken(){
+    const wallet = useWallet();
+    const connection = useConnection();
+
+
+
+
+    async function createToken(){
         const name = document.getElementById("name").value;
         const symbol = document.getElementById("symbol").value;
         const imageUrl = document.getElementById("image-url").value;
         const initialSupply = document.getElementById("initial-supply").value;
         console.log(name);
+        
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+        const keypair = Keypair.generate()
+
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: keypair.publicKey,
+                space: MINT_SIZE,
+                lamports,
+                programId: TOKEN_PROGRAM_ID,
+            }),
+            createInitializeMint2Instruction(keypair.publicKey, 9, wallet.publicKey, wallet.publicKey, TOKEN_PROGRAM_ID)
+        );
+
+        transaction.partialSign(keypair);
+        const response = await wallet.sendTransaction(transaction);
+        console.log(response);
+
     }
 
     return  <div style={{
